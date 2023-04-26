504043268c62bb94e82b5d41f4e579bfb8f183ab
Mahmoud-Al-Kowatli <121610661+Mahmoud-Al-Kowatli@users.noreply.github.com>
2023-04-26 14:06:13 +0300

    Add-project-v002

---
+++ myHomeWorkForm/Mod/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace myhomework.Model
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Registerdate { get; set; }
+        public int Department_id { get; set; }
+
+
+    }
+}
---
+++ myHomeWorkForm/MyCont/MyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myhomework.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+
+
+namespace myHomeWorkForm.MyCont
+{
+    public class MyDbContext : DbContext
+    {
+
+        //public MyDbContext() : base("name=MyDbContext")
+        //{
+
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-DOI5329;Initial Catalog=homeworkDB;Integrated Security=True; TrustServerCertificate=True ");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public virtual DbSet<Student> students1 { get; set; }
+    }
+}
